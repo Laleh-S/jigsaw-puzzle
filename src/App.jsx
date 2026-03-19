138c9af1bbb9649e03213dba1d2c4bf58686f88a
Laleh Shahidi <laleh.shahidi78@gmail.com>
2026-03-19 07:49:26 +0000

    move from one piece state to array of pieces state

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ function App() {
   // next state: whatever you pass into setPiece1Pos(...) 
   // setPiece1Pos is the function that allows you make these changes
 
-  const [piece1Pos, setPiece1Pos] = useState({ x: 0, y: 0 }) // Position lives here
+  const [pieces, setPieces] = useState([  // Position lives here
+    { id: 1, x: 0, y: 0 },
+    { id: 2, x: 10, y: 10 },
+  ]) 
   const [isDragging, setIsDragging] = useState(false)  // On and off switch for dragging
   
   const targetPos = { x: 200, y: 200 }
@@ -28,7 +31,12 @@ function App() {
       <h1>Jigsaw Puzzle</h1>
       <button
         onClick={() => {
-          setPiece1Pos({ x: startPos.x, y: startPos.y })
+          setIsDragging(false)
+          setPieces([
+            // ...pieces[0] means: copy all fields from the first piece object (for example: { id: 1, x: 0, y: 0 })
+            { ...pieces[0], x: startPos.x, y: startPos.y }, 
+            pieces[1], // stays unchanged
+          ])
         }}
       >
         Reset
@@ -39,13 +47,16 @@ function App() {
         onMouseUp={() => {  // stop dragging (mouse button comes up)
           setIsDragging(false)
           // Measure how far the piece is from the target (left/right) or (up/down)
-          const distanceX = Math.abs(piece1Pos.x - targetPos.x)
-          const distanceY = Math.abs(piece1Pos.y - targetPos.y)
+          const distanceX = Math.abs(pieces[0].x - targetPos.x)
+          const distanceY = Math.abs(pieces[0].y - targetPos.y)
           
           // if it’s “close enough” (within 20 pixels in both directions)
           if (distanceX < 20 && distanceY < 20) {
             // if yes, snap it perfectly into place
-            setPiece1Pos({ x: targetPos.x, y: targetPos.y })
+            setPieces([
+              { ...pieces[0], x: targetPos.x, y: targetPos.y },
+              pieces[1],
+            ])
             clickSound.play()
           }
         }}
@@ -55,11 +66,14 @@ function App() {
           // console.log(event)
           if (isDragging) { // only if we are draging do the followings
             // 220 value comes from the piece-container minus piece 
-            const nextX = piece1Pos.x + event.movementX // Take the current x position of the piece. Add how far the mouse just moved left/right. This is where the piece wants to go next horizontally.
-            const nextY = piece1Pos.y + event.movementY // Same idea for y (up/down).
+            const nextX = pieces[0].x + event.movementX // Take the current x position of the piece. Add how far the mouse just moved left/right. This is where the piece wants to go next horizontally.
+            const nextY = pieces[0].y + event.movementY // Same idea for y (up/down).
             const clampedX = clamp(nextX, 0, 220) // If nextX is less than 0, use 0. If nextX is more than 220, use 220. Otherwise use nextX. → keeps the piece inside horizontally.
             const clampedY = clamp(nextY, 0, 220) // Same, but for the vertical direction.
-            setPiece1Pos({ x: clampedX, y: clampedY }) // Save the new (possibly limited) position.
+            setPieces([
+              { ...pieces[0], x: nextX, y: nextY },
+              pieces[1],
+            ])
           }
         }}
       >
@@ -69,7 +83,7 @@ function App() {
         {/* draggable piece */}
         <div
           className="piece"  // put the onMouseDown on the piece since dragging should start only when the piece is clicked, not the box.
-          style={{ top: piece1Pos.y, left: piece1Pos.x }}
+          style={{ top: pieces[0].y, left: pieces[0].x  }}
 
           onMouseDown={() => {  // start dragging (mouse button goes down)
             setIsDragging(true);
@@ -84,4 +98,5 @@ function App() {
   )
 }
 export default App
+
 
